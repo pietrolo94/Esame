95802a2e4a730adff3772ff97b4ad18e71f70f78
pietrolo94 <salamoumayma@gmail.com>
2023-05-26 15:06:00 +0200

    sdgsdf

---
+++ app.py
@@ -0,0 +1,77 @@
+import streamlit as st
+import pandas as pd
+import joblib
+import io
+
+def add_bg_from_url():
+    st.markdown(
+         f"""
+         <style>
+         .stApp {{
+             background-image: url("https://w.wallhaven.cc/full/96/wallhaven-968zwd.jpg");
+             background-attachment: fixed;
+             background-size: cover
+         }}
+         </style>
+         """,
+         unsafe_allow_html=True
+     )
+
+def main():
+    st.markdown('<div align="center"><h1>Aspettativa di vita</h1></div>', unsafe_allow_html=True)
+
+    newmodel = joblib.load("Life_exp.pkl")
+    adult_mortality = st.number_input("Adult mortality")
+    hiv_aids = st.number_input("HIV or AIDS")
+    income = st.number_input("Income composition of resources")
+    schooling = st.number_input("Schooling")
+    res = newmodel.predict([[adult_mortality,hiv_aids, income, schooling]])[0]
+    st.write(f"aspettativa di vita: {round(res, 1)} anni")
+
+
+    # Parte per caricare il file CSV o Excel
+    st.header("Caricamento dati")
+    file = st.file_uploader("Carica un file CSV o Excel", type=["csv", "xlsx"])
+    if file is not None:
+        if file.type.startswith('application/vnd.openxmlformats-officedocument.spreadsheetml'):
+            df = pd.read_excel(file, engine='openpyxl')
+        else:
+            df = pd.read_csv(file)
+        dfx = df.drop(columns=['infant deaths', 'percentage expenditure',
+ 								'Hepatitis B',
+ 								'Measles ',
+ 								' BMI ',
+								'under-five deaths ',
+								'Polio',
+								'Diphtheria ',
+								' thinness  1-19 years',
+								' thinness 5-9 years',
+                                'Country',
+                                'Year',
+                                'Life_exp']
+                                )
+        # Mostra i dati caricati
+        st.write("Dati caricati:")
+        st.write(df)
+
+        # Previsione dei dati usando il modello di regressione lineare
+        st.header("Previsione aspettativa di vita")
+        predictions = newmodel.predict(dfx)
+        df['Predizione aspettativa di vita'] = predictions
+        st.write("Risultati previsione:")
+        st.write(df)
+        # Aggiungi un pulsante per il download del file
+        output = io.BytesIO()
+        writer = pd.ExcelWriter(output, engine='xlsxwriter')
+        df.to_excel(writer,sheet_name='Profit_prediction', index=False)
+        writer.save()
+        output.seek(0)
+        st.download_button(
+            label="Scarica file Excel",
+            data=output,
+            file_name='Life_exp_prediction.xlsx',
+            mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+    )
+    add_bg_from_url()
+if __name__ == '__main__':
+	main()
